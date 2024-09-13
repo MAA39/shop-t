c0a97aaaca2bf3de567f5324a7748db9b5a5e5f9
MAA39 <masa.nekoshinshi39@gmail.com>
2024-09-13 17:40:46 +0900

    chore: First Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +17,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="ja" suppressHydrationWarning>
+      <body className={inter.className}>
+        <header className="border-b conitems-center h-16 flex px-6">
+          <div className="container flex">
+            <Button
+              className="items-center font-bold text-lg"
+              asChild
+              variant={"ghost"}>
+              <Link href="/">高橋商店</Link>
+            </Button>
+          </div>
+        </header>
+        <main>{children}</main>
+        <footer className="border-t items-center h-16 flex px-6">
+          <div className="container ">
+            <p>© 2024 高橋商店</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
---
+++ config.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "高橋商店",
+  description: "高橋商店のサイトです。",
+};
